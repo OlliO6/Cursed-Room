ec3375c6dfdabd92ae0b90aaf8589363ee867ebb
OlliDev <oliver.koenig.bi@gmail.com>
2024-03-12 19:42:02 +0100

    add attack and spezial input

--- singletons/input_manager.gd
+++ singletons/input_manager.gd
@@ -2,6 +2,11 @@ extends Node
 
 const DEADZONE:= 0.3
 
+signal attack_started
+signal attack_ended
+signal spezial_started
+signal spezial_ended
+
 func get_move_input() -> Vector2:
 	
 	var result: Vector2
@@ -12,3 +17,16 @@ func get_move_input() -> Vector2:
 		result = result.normalized()
 		
 	return result if lenght > DEADZONE else Vector2.ZERO
+
+func _unhandled_input(event: InputEvent) -> void:
+	
+	if event.is_echo():
+		return
+	
+	if event.is_action("attack"):
+		(attack_started if event.is_pressed() else attack_ended).emit()
+		return
+	
+	if event.is_action("spezial"):
+		(spezial_started if event.is_pressed() else spezial_ended).emit()
+		return
