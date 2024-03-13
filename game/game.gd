ed7685b435d57ca1645f8f45ee6ff598883f7cb5
OlliDev <oliver.koenig.bi@gmail.com>
2024-03-14 00:01:23 +0100

    disable light in friendly zone

--- game/game.gd
+++ game/game.gd
@@ -11,20 +11,27 @@ signal deadly_zone_entered
 
 func _ready() -> void:
 	InputManager.switch_zone_pressed.connect(switch_zone)
+	friendly_zone.show()
+	deadly_zone.show()
 	enter_zone(_current_zone)
 
+func in_friendly_zone() -> bool:
+	return _current_zone == Zone.Friendly
+
 func switch_zone() -> void:
-	enter_zone(Zone.Deadly if _current_zone == Zone.Friendly else Zone.Friendly)
+	enter_zone(Zone.Deadly if in_friendly_zone() else Zone.Friendly)
 
 func enter_zone(zone: Zone) -> void:
 	_current_zone = zone
 
-	var to_remove := deadly_zone if zone == Zone.Friendly else friendly_zone
-	var to_add := friendly_zone if zone == Zone.Friendly else deadly_zone
+	var to_remove := deadly_zone if in_friendly_zone() else friendly_zone
+	var to_add := friendly_zone if in_friendly_zone() else deadly_zone
 
 	if to_remove.is_inside_tree():
 		remove_child(to_remove)
 	if !to_add.is_inside_tree():
 		add_child(to_add)
+	
+	(friendly_zone_entered if in_friendly_zone() else deadly_zone_entered).emit()
 
 enum Zone {Friendly, Deadly}
